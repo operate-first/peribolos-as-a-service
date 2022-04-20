01128d3646841a1264bbba654db10e35907c987b
Michal Drla <mdrla@redhat.com>
2022-04-20 14:34:42 +0200

    feat: Add script to build an image
    Signed-off-by: Michal Drla <mdrla@redhat.com>

---
+++ scripts/build-image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Base image
+NODEJS_BASE_IMAGE=$(cat .aicoe-ci.yaml | grep -oP '(?<=base-image: ).*')
+
+# Checks whether the important programs are installed
+if ! command -v podman &> /dev/null; then
+  echo "This script requires podman to be installed."
+  exit 1
+fi
+if ! command -v s2i &> /dev/null; then
+  echo "This script requires s2i to be installed."
+  exit 1
+fi
+# Creates temporary folder for the build
+tmp_dir=$(mktemp -d -t peribolos-XXXXXXXXXX)
+
+s2i build . ${NODEJS_BASE_IMAGE} --as-dockerfile ${tmp_dir}/Containerfile
+cd $tmp_dir
+podman build -t peribolos-as-a-service .
+
+# Cleaning after the build
+rm -rf $tmp_dir
